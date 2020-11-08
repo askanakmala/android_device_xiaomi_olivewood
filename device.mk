9f80c1b006dc79bd38e28c8c12120b76650f89ef
Askan28 <46845990+askanakmala@users.noreply.github.com>
2020-11-08 18:11:57 +0700

    olivewood: added /sensors/sensor_def_qcomdev.conf
    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf

--- device.mk
+++ device.mk
@@ -256,6 +256,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Securelock
 PRODUCT_COPY_FILES += \
